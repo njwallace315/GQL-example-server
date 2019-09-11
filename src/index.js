89d1280ca0e976eebfbd706a124b8e2947e063c3
Nathaniel Wallace <nate.wallace@brms.wisc.edu>
2019-09-11 12:20:43 -0500

    finish resolvers and mutations

--- src/index.js
+++ src/index.js
@@ -1,35 +1,57 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
-      
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`
-
 let links = [{ id: "link-0", url: 'www.howtographql.com', description: "Fullstack tutorial for GraphQL" }]
-
+let idCount = links.length;
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links,
     },
-    Link: {
-        id: ({ id }) => id,
-        url: ({ url }) => url,
-        description: ({ description }) => description,
+    Mutation: {
+        post: (parent, { url, description }) => {
+            const link = {
+                id: `link-${idCount++}`,
+                url,
+                description,
+            }
+            links.push(link)
+            return link;
+        },
+        updateLink: (parent, { id, description, url }) => {
+            let link;
+            links = links.map(x => {
+                if (x.id === id) {
+                    link = { id, url: url || x.url, description: description || x.description }
+                    return link
+                }
+                return x
+            })
+            return link
+        },
+        deleteLink: (parent, { id }) => {
+            let link;
+            links = links.filter(x => {
+                if (x.id === id) {
+                    link = { ...x };
+                    return false;
+                }
+                return true
+            });
+            return link
+        }
 
-    }
+    },
+
+    /* This is implicit */
+    // Link: {
+    //     id: ({ id }) => id,
+    //     url: ({ url }) => url,
+    //     description: ({ description }) => description,
+    // }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
 server.start(() => { console.log('GraphQL server is running on http://localhost:4000') })
